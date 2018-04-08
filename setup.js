0d8daae577da91dc60fcf6b6b07f39ed2c556de3
bieniajl <j.bienias@web.de>
2018-04-08 12:54:47 +0200

    Add the calender setup machine

--- setup.js
+++ setup.js
@@ -18,30 +18,78 @@ conn.connect(
         else {
             console.log("Connection established.");
 
-            var sql = "DROP DATABASE calendarDb;";
+            var sql = "DROP DATABASE IF EXISTS calendarDb;";
 
             conn.query(sql, function (err, results, fields) {
                 if (err) {
                     throw err;
                 }
             });
-            sql = [
-                "CREATE Database calendarDb;",
-                "USE calendarDb CREATE TABLE `Calendar` (`id` varchar(50) NOT NULL AUTO_INCREMENT,`name` TEXT NOT NULL, `description` TEXT NOT NULL,PRIMARY KEY (`id`));",
-                "USE calendarDb CREATE TABLE `Notification` (`id` varchar NOT NULL,`appointmentId` varchar(50) NOT NULL UNIQUE,`notification` TEXT NOT NULL UNIQUE,`time` TIMESTAMP NOT NULL,PRIMARY KEY (`id`,`appointmentId`));",
-                "USE calendarDb CREATE TABLE `Appointment` (`id` varchar(50) NOT NULL,`calendarID` varchar(50) NOT NULL,`creatorId` varchar(50) NOT NULL,`Name` TEXT NOT NULL,`description` TEXT NOT NULL,`location` TEXT NOT NULL,`startDate` DATE NOT NULL,`endDate` DATE NOT NULL,PRIMARY KEY (`id`));",
-                "USE calendarDb CREATE TABLE `UserAppointment` (`id` varchar(50) NOT NULL, `userId` varchar(50) NOT NULL,`appointmentId` varchar(50) NOT NULL,`state` varchar(50) NOT NULL,PRIMARY KEY (`userId`,`appointmentId`, `id`));",
-                "ALTER TABLE `Notification` ADD CONSTRAINT `Notification_fk0` FOREIGN KEY (`appointmentId`) REFERENCES `Appointment`(`id`);",
-                "ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk0` FOREIGN KEY (`calendarID`) REFERENCES `Calendar`(`id`);",
-                "ALTER TABLE `UserAppointment` ADD CONSTRAINT `UserAppointment_fk0` FOREIGN KEY (`appointmentId`) REFERENCES `Appointment`(`id`);",
-            ];
-            for (i in sql) {
-                conn.query(i, function (err, results, fields) {
-                    if (err) {
-                        throw err;
-                    }
-                });
-            }
+
+
+            sql = "CREATE Database calendarDb;"
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "USE calendarDb"
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `Calendar` (`id` varchar(50) NOT NULL ,`name` TEXT NOT NULL, `description` TEXT NOT NULL,PRIMARY KEY (`id`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `Notification` (`id` varchar(50) NOT NULL,`appointmentId` varchar(50) NOT NULL UNIQUE,`notification` TEXT NOT NULL,`time` TIMESTAMP NOT NULL,PRIMARY KEY (`id`,`appointmentId`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `Appointment` (`id` varchar(50) NOT NULL,`calendarID` varchar(50) NOT NULL,`creatorId` varchar(50) NOT NULL,`Name` TEXT NOT NULL,`description` TEXT NOT NULL,`location` TEXT NOT NULL,`startDate` DATE NOT NULL,`endDate` DATE NOT NULL,PRIMARY KEY (`id`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "CREATE TABLE `UserAppointment` (`id` varchar(50) NOT NULL, `userId` varchar(50) NOT NULL,`appointmentId` varchar(50) NOT NULL,`state` varchar(50) NOT NULL,PRIMARY KEY (`userId`,`appointmentId`, `id`));";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "ALTER TABLE `Notification` ADD CONSTRAINT `Notification_fk0` FOREIGN KEY (`appointmentId`) REFERENCES `Appointment`(`id`);";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk0` FOREIGN KEY (`calendarID`) REFERENCES `Calendar`(`id`);";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+            });
+
+            sql = "ALTER TABLE `UserAppointment` ADD CONSTRAINT `UserAppointment_fk0` FOREIGN KEY (`appointmentId`) REFERENCES `Appointment`(`id`);";
+            conn.query(sql, function (err, results, fields) {
+                if (err) {
+                    throw err;
+                }
+                conn.end();
+            });
         }
     });
 
